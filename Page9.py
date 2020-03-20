5130bcd5ecac8164f0ca9bcab9ef2d3cf8b672f4
nchin94 <54481729+nchin94@users.noreply.github.com>
2020-03-20 14:21:50 -0400

    Add files via upload

---
+++ Page9.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[6]:
+
+
+location = "C:\\Users\\nchin\\Data Viz\\Week7_files\\LND01.xls"
+
+
+# In[7]:
+
+
+df = pd.read_excel(location)
+
+
+# In[8]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
